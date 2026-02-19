b0628d1f0497855ad3aab8fc424fdf138a211b21
CJ <karolmmm01@gmail.com>
2026-02-19 21:41:42 +0100

    238 Solution

--- 238_Product_of_Array_Except_Self.py
+++ 238_Product_of_Array_Except_Self.py
@@ -1,28 +1,63 @@
+# class Solution:
+#     def productExceptSelf(self, nums: list[int]) -> list[int]:
+#         prefix = []
+#         for i, num in enumerate(nums):
+#             if i == 0:
+#                 temp = num
+#             else:
+#                 temp = temp * num
+#             prefix.append(temp)
+#         postfix = [None] * len(nums)
+#         for i in range(len(nums) - 1, -1, -1):
+#             if i == len(nums) - 1:
+#                 temp = nums[i]
+#             else:
+#                 temp = temp * nums[i]
+#             postfix[i] = temp
+
+#         res = []
+#         for i, num in enumerate(nums):
+#             if i == 0:
+#                 res.append(postfix[1])
+#             elif i == len(nums) - 1:
+#                 res.append(prefix[i-1])
+#             else:
+#                 res.append(prefix[i - 1] * postfix[i + 1])
+#         return res
+
+# test = Solution().productExceptSelf(nums = [4,3,2,1,2])
+
+
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
-        prefix = []
+        n = len(nums)
+
+        after = [1] * n
+        before = [1] * n
+        result = [1] * n
+
         for i, num in enumerate(nums):
             if i == 0:
-                temp = num
+                after[0] = 1
             else:
-                temp = temp * num
-            prefix.append(temp)
-        postfix = [None] * len(nums)
-        for i in range(len(nums) - 1, -1, -1):
-            if i == len(nums) - 1:
-                temp = nums[i]
-            else:
-                temp = temp * nums[i]
-            postfix[i] = temp
+                after[i] = after[i - 1] * nums[i - 1]
 
-        res = []
-        for i, num in enumerate(nums):
-            if i == 0:
-                res.append(postfix[1])
-            elif i == len(nums) - 1:
-                res.append(prefix[i-1])
+        for i in range(n - 2, -1, -1):
+            print(i)
+            if i == n:
+                before[n] = 1
             else:
-                res.append(prefix[i - 1] * postfix[i + 1])
-        return res
+                before[i] = nums[i + 1] * before[i + 1]
 
-test = Solution().productExceptSelf(nums = [4,3,2,1,2])
+        # before or after in enumarete doesen't matter
+        for i, num in enumerate(before):
+            result[i] = before[i] * after[i]
+
+        return result
+
+        print(after)
+        print(before)
+        print(result)
+
+
+asd = Solution().productExceptSelf([1, 2, 4, 6])
