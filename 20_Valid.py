56d0c50c1cef0e0c4d777c9d8046d65e977d10e9
CJ <karolmmm01@gmail.com>
2026-04-08 23:26:26 +0200

    20 Solution

--- 20_Valid.py
+++ 20_Valid.py
@@ -18,3 +18,24 @@ class Solution:
 
 
 asd = Solution().isValid("([)]")
+
+
+class Solution2:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {"}": "{", "]": "[", ")": "("}
+
+        for el in s:
+            if stack and el in mapping and stack[-1] == mapping[el]:
+                stack.pop()
+            else:
+                stack.append(el)
+
+        if len(stack) == 0:
+            return True
+        else:
+            return False
+
+
+asd = Solution2().isValid("[(])")
+print(asd)
